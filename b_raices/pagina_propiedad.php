83b2f66d331e3f834e858baee8c6629f64fa26e6
JukaBotaz <juancanog12@gmail.com>
2025-04-11 04:21:58 -0600

    Quedo el front y funcionalidad

--- b_raices/pagina_propiedad.php
+++ b_raices/pagina_propiedad.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Propiedades</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
   <style>
     .propiedad { margin-bottom: 40px; }
     .galeria img { width: 200px; margin-right: 10px; }
@@ -10,11 +12,17 @@
 
 </head>
 <body>
+        <h1>Contacto Whatsapp</h1>
+        <a href="https://wa.me/526444573361" class="boton_whatsapp">
+            <i class="fab fa-whatsapp"></i> WhatsApp
+        </a>
 
   <h1>Lista de Propiedades</h1>
   <?php include('probar.php'); ?>
-  <h2>Deja tu comentario</h2>
+  
     <form action="formulario.php" method="POST">
+    <h1>Contactar un Asesor</h1>
+    <hr class="formulario__linea">
         <label>Nombre:</label><br>
         <input type="text" name="nombre" required><br><br>
 
