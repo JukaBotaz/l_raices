b128effe2aaf6e083be30dd56bf4499c112262c8
JukaBotaz <juancanog12@gmail.com>
2025-04-11 00:00:03 -0600

    Ahora si, conexion base de datos

---
+++ b_raices/conexion.php
@@ -0,0 +1,15 @@
+<?php
+// Datos de conexión a tu base local en XAMPP
+$host = "localhost";
+$usuario = "root";
+$clave = ""; // En XAMPP, root no tiene contraseña por defecto
+$bd = "bienes_raices"; // Asegúrate que se llama igual tu base de datos
+
+// Conectar a la base de datos
+$conn = new mysqli($host, $usuario, $clave, $bd);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
