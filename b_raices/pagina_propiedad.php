442d209002c12a0e49942b85b9a689c8896d8536
KrisHarms <kristina.rodriguez.e@lasallenoroeste.edu.mx>
2025-04-11 03:14:38 -0600

    Formulario funcionalidad

--- b_raices/pagina_propiedad.php
+++ b_raices/pagina_propiedad.php
@@ -13,5 +13,22 @@
 
   <h1>Lista de Propiedades</h1>
   <?php include('probar.php'); ?>
+  <h2>Deja tu comentario</h2>
+    <form action="formulario.php" method="POST">
+        <label>Nombre:</label><br>
+        <input type="text" name="nombre" required><br><br>
+
+        <label>Correo:</label><br>
+        <input type="email" name="correo" required><br><br>
+
+        <label>Celular:</label><br>
+        <input type="text" name="celular" required><br><br>
+
+        <label>Comentario:</label><br>
+        <textarea name="comentario" rows="5" required></textarea><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+    <?php include('formulario.php'); ?>
 </body>
 </html>
