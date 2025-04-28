41771044423cf4269850d413e18e98a1d40d4e97
JukaBotaz <juancanog12@gmail.com>
2025-04-27 23:48:21 -0600

    Nuevo archivo php, para mapa

--- b_raices/pagina_propiedad.php
+++ b_raices/pagina_propiedad.php
@@ -20,6 +20,13 @@
   <h1>Lista de Propiedades</h1>
   <?php include('probar.php'); ?>
   
+  <h1>Ubicación en Mapa</h1>
+  <hr>
+  <p>Caffenio de la Nainari y Sinaloa</p>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4208.611476650537!2d-109.93486433790298!3d27.500179405813704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86c815eca0795b71%3A0xca26f8154fda8b46!2sCaffenio!5e0!3m2!1sen!2smx!4v1745809948072!5m2!1sen!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
     <form action="formulario.php" method="POST">
     <h1>Contactar un Asesor</h1>
     <hr class="formulario__linea">
